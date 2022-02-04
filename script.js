418039c13ecae5d1d13578eb110c0d3b251feb4d
matassalys0 <matassalys@gmail.com>
2022-02-04 23:07:24 +0200

    feat: initial div generation

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 "use strict"
 
+const mainContainer = document.querySelector('.container');
+//Setting the height for 90% of the screen  
+const divHeight = 0.9 * window.innerHeight / 16;
+const colWidth = 0.9 * window.innerWidth / 16;
+
+//for loop is used to create the rows
+for(let i = 0; i <= 15; i++) {
+
+    let rowDiv = document.createElement('div');
+    rowDiv.style.height =  divHeight + "px";
+    rowDiv.classList.add('container-row');
+    rowDiv.id = 'row-' + i;
+    mainContainer.appendChild(rowDiv);
+
+    //for loop is used to add divs to the rows
+    for(let j = 0; j <= 15; j++) {
+        let colDiv = document.createElement('div');
+        colDiv.style.width = colWidth + 'px';
+        colDiv.id = 'row-' + i + 'col-' + j;
+        rowDiv.appendChild(colDiv);
+    }
+}
