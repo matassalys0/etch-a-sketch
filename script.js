baccb287f9855bfaf5f77aa38563192e4e84cd0a
matassalys0 <matassalys@gmail.com>
2022-02-06 19:39:42 +0200

    feat:grid size changeable with slider

--- script.js
+++ script.js
@@ -3,15 +3,21 @@
 const etchContainer = document.querySelector('.etch-container');
 //Setting the height for 90% of the screen  
 let gridSize = 16;
-const divHeight = etchContainer.clientHeight / gridSize;
-const colWidth =  etchContainer.clientWidth / gridSize;
-console.log(etchContainer.clientHeight);
+let divHeight = etchContainer.clientHeight / gridSize;
+let colWidth =  etchContainer.clientWidth / gridSize;
+
 //this for loop is used to create the rows
 function paintDiv(e) {
     e.target.style.backgroundColor = 'black';
 }
 
-function createEtch() {
+function createEtch(e = undefined) {
+    if(e != undefined && e.srcElement.classList.contains('slider')) {
+        gridSize = e.srcElement.value;
+        divHeight = etchContainer.clientHeight / gridSize;
+        colWidth =  etchContainer.clientWidth / gridSize;
+    }
+    
     etchContainer.innerHTML = "";
     for(let i = 0; i <= gridSize - 1; i++) {
 
@@ -62,3 +68,13 @@ createEtch();
 const clearBtn = document.querySelector('.clear-btn');
 clearBtn.addEventListener('click', createEtch);
 
+const sliderValue = document.querySelector('.slider-value');
+sliderValue.textContent = `${gridSize}x${gridSize}`;
+
+const slider = document.querySelector('.slider');
+slider.addEventListener('input', sliderChange);
+
+function sliderChange(e) {
+    sliderValue.textContent = `${e.srcElement.value}x${e.srcElement.value}`;
+    createEtch(e);
+}
